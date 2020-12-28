8eeb22384429d59cb797d45ef05f18e8028569a6
heartoftrailers <ciovicaflorin0@gmail.com>
2020-12-28 00:33:22 +0000

    email login full working

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Trip)
 
 # Register your models here.
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group
 
+admin.site.unregister(Group)
 admin.site.register(Driver)
 admin.site.register(Company)
+admin.site.register(User)
